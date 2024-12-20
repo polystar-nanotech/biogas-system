1c827f7ec39cfadad6e363d136a7a3dfbd358ce3
sauverpro <unsauvechriss@gmail.com>
2024-12-20 15:57:45 +0200

    adding tesmonials and map

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { ChangePassword, CreateUser, LoginUser } from '../controller/auth';
 import { ValidateData, ValidateLoginData } from '../middleware';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export const AuthRouter = Router();
 
